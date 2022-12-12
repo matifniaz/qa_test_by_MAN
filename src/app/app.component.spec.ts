9772601f02c99225d74447b3ea5a9b86f7d9bdc7
andylitahh <andy.lita@outlook.de>
2022-12-12 13:21:42 +0100

    removed training span

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('training');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('training app is running!');
-  });
 });
